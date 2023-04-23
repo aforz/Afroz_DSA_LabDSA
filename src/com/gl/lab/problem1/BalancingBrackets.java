c42d85cda0542460aababc88358d609aee75971b
Afroz Alam <afroz3002@gmail.com>
2023-04-23 11:03:26 +0530

    Lab3 DSA Fab23

---
+++ src/com/gl/lab/problem1/BalancingBrackets.java
@@ -0,0 +1,47 @@
+package com.gl.lab.problem1;
+
+import java.util.Stack;
+
+public class BalancingBrackets {
+
+    public static void main(String[] args) {
+//        String input = "([[{}]])";
+        String input = "([[{}]]))";
+        if (isInputBracketsBalanced(input))
+            System.out.println("The entered String has Balanced Brackets");
+        else
+            System.out.println("The entered Strings do not contain Balanced Brackets");
+    }
+
+    private static boolean isInputBracketsBalanced(String input) {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+                continue;
+            }
+            if (stack.isEmpty())
+                return false;
+            char bracket;
+            switch (ch) {
+                case ')':
+                    bracket = stack.pop();
+                    if (bracket == '{' || bracket == '[')
+                        return false;
+                    break;
+                case '}':
+                    bracket = stack.pop();
+                    if (bracket == '(' || bracket == '[')
+                        return false;
+                    break;
+                case ']':
+                    bracket = stack.pop();
+                    if (bracket == '(' || bracket == '{')
+                        return false;
+                    break;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
---
+++ src/com/gl/lab/problem2/FindPairInBST.java
@@ -0,0 +1,93 @@
+package com.gl.lab.problem2;
+
+class Node {
+    int key;
+    Node left, right;
+
+    Node(int data) {
+        key = data;
+        left = null;
+        right = null;
+    }
+}
+
+public class FindPairInBST {
+    Node root;
+
+    FindPairInBST() {
+        root = null;
+    }
+
+    void insert(int key) {
+        root = insertRecursively(root, key);
+    }
+
+    Node insertRecursively(Node root, int data) {
+        if (root == null) {
+            root = new Node(data);
+            return root;
+        }
+        if (data < root.key) {
+            root.left = insertRecursively(root.left, data);
+        } else if (data > root.key) {
+            root.right = insertRecursively(root.right, data);
+        }
+        return root;
+    }
+
+    boolean isPairAvailable(Node root, Node temp, int sum) {
+        if (temp == null)
+            return false;
+        else
+            return search(root, temp, sum - temp.key) || isPairAvailable(root, temp.left, sum) || isPairAvailable(root, temp.right, sum);
+    }
+
+    boolean search(Node root, Node temp, int key) {
+        if (root == null)
+            return false;
+        Node current = root;
+        boolean flag = false;
+        while (current != null && flag != true) {
+            if (current.key == key && temp != current) {
+                flag = true;
+                System.out.println("Pair is (" + current.key + "," + temp.key + ")");
+                return true;
+            } else if (key < current.key)
+                current = current.left;
+            else
+                current = current.right;
+        }
+        return flag;
+    }
+
+    public static void main(String[] args) {
+        FindPairInBST findPairInBST = new FindPairInBST();
+        /*
+                   4
+                /     \
+              2        6
+             / \      /  \
+            1   3    5    7
+        */
+//        findPairInBST.insert(4);
+//        findPairInBST.insert(2);
+//        findPairInBST.insert(6);
+//        findPairInBST.insert(1);
+//        findPairInBST.insert(3);
+//        findPairInBST.insert(5);
+//        findPairInBST.insert(7);
+        findPairInBST.insert(15);
+        findPairInBST.insert(10);
+        findPairInBST.insert(20);
+        findPairInBST.insert(8);
+        findPairInBST.insert(12);
+        findPairInBST.insert(16);
+        findPairInBST.insert(25);
+
+        System.out.println("Sum = " + 35);
+        if (findPairInBST.isPairAvailable(findPairInBST.root, findPairInBST.root, 35)) {
+
+        } else
+            System.out.println("nodes are not found.");
+    }
+}
